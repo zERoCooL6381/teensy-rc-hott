e2f43e256c032dd62d304192846e87c4f27019ef
cord johannmeyer <cord.johannmeyer@t-online.de>
2013-04-23 00:39:15 +0200

    added PID control

--- src/SerialTest.cpp
+++ src/SerialTest.cpp
@@ -2,6 +2,7 @@
 #include "PITimer.h"
 #include "lib_crc.h"
 #include "Servo.h"
+#include "PID_v1.h"
 
 /* HOTT Read, read serial data from Graupner HOTT GR-32 receiver
    by Cord Johannmeyer
@@ -12,7 +13,7 @@
 */
 
 
-const int ledPin = 13;   // Teensy 3.0 on board LED
+const int ledPin = LED_BUILTIN;   // Teensy 3.0 on board LED (13)
 const int rpmPin = 3;    // for RPM sensor input
 
 enum eSerialState { WaitStart, ReadHeader, ReadData, ReadCrc };
@@ -56,13 +57,23 @@ struct tSerData {
 	tSerData() : state(WaitStart), cnt(0) {};
 } serData;
 
+struct tPIDParam {
+	double in, out, set;
+	double kp, ki, kd;
+	int dir;
+	double rmin, rmax;
+	tPIDParam(): in(0.0), out(0.0), set(0.0), kp(1.0), ki(0.0), kd(0.0), dir(0), rmin(1000.0), rmax(5000.0) {};
+} PIDParam;
+
 unsigned char* pRawBuffer = (unsigned char* )&(serData.rxFrame);
 volatile uint32_t current;
 volatile uint32_t lastPulseEvent;
 volatile uint32_t diffTime;
 
 Servo servo_ch0;
-//Servo servo_ch1;
+Servo servo_ch1;
+
+PID rpmCtrl(&PIDParam.in, &PIDParam.out, &PIDParam.set, PIDParam.kp, PIDParam.ki, PIDParam.kd, PIDParam.dir);
 
 void timerCallback0() {
 	serData.cnt = 0;
@@ -71,6 +82,16 @@ void timerCallback0() {
 }
 
 void timerCallback1() {
+}
+
+double calcSetpoint(double in)
+{
+	return PIDParam.rmin + (PIDParam.rmax - PIDParam.rmin) * (in - 1000.0) / 1000.0 ;
+}
+
+int calcOutput(double out)
+{
+	return (int)1000 + 1000.0 * (out - PIDParam.rmin) / (PIDParam.rmax - PIDParam.rmin);
 }
 
 void rpm_isr()
@@ -97,8 +118,13 @@ void setup()   {
 	PITimer1.start(timerCallback1);
 
 	attachInterrupt(rpmPin, rpm_isr, FALLING);
+
+	rpmCtrl.SetSampleTime(10);
+	rpmCtrl.SetOutputLimits(1000.0, 5000.0);
+	rpmCtrl.SetMode(1);
+
 	servo_ch0.attach(4);
-//	servo_ch0.attach(5);
+	servo_ch1.attach(5);
 }
 
 unsigned short crc = 0;
@@ -125,30 +151,42 @@ void loop()
 
 		uint32_t rpm = (uint32_t(F_CPU)*60)/diffTime;
 
-		Serial.print(" rpm: ");
-		Serial.print(rpm);
+		double ch0 = 1000.0;
 
 		if(crc == serData.rxFrame.crc) {
-			float ch0 = serData.rxFrame.rxChannel[0] * 0.2 - 900.0;
-			Serial.print(" ch0: ");
+			ch0 = serData.rxFrame.rxChannel[0] * 0.2 - 900.0;
+			Serial.print("$ ");
+			Serial.print(rpm);
+			Serial.print(" ");
 			Serial.print(ch0);
 			if(ch0 > 1500.0) 	digitalWrite(ledPin, HIGH);   // set the LED on
 			else	digitalWrite(ledPin, LOW);
 
 //			analogWrite(4,int(0.255*(ch0-1000.0)));
-			servo_ch0.writeMicroseconds(int(ch0));
-//			servo_ch1.writeMicroseconds(int(ch0)+100);
+//			servo_ch0.writeMicroseconds(int(ch0));
+			servo_ch1.writeMicroseconds(int(ch0));
 
-			Serial.println(" ok");
+//			Serial.println(" ok");
 		}
-		else {
-			Serial.println(" err");
-		}
-//		Serial.print(rxFrame->crc, HEX);
-//		Serial.print("!");
-//		Serial.println(crc,HEX);
+//		else {
+//			Serial.println(" err");
+//		}
 
 		serData.cnt = 0;
 		crc = 0;
+
+		// PID controller
+
+		PIDParam.in = (double)rpm;
+		PIDParam.set = calcSetpoint(ch0);
+		rpmCtrl.Compute();
+		int ch0_out = calcOutput(PIDParam.out);
+		servo_ch0.writeMicroseconds(ch0_out);
+		Serial.print(" ");
+		Serial.print(PIDParam.set);
+		Serial.print(" ");
+		Serial.println(ch0_out);
+
+
 	}
 }
